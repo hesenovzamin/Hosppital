19ecf5a6546807d324afc79be5c9ad32ea89da70
hesenovzamin <hesenovzamin@gmail.com>
2017-08-22 12:33:28 +0400

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button30 = new System.Windows.Forms.Button();
+            this.button31 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button33 = new System.Windows.Forms.Button();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.button34 = new System.Windows.Forms.Button();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.button35 = new System.Windows.Forms.Button();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.button36 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button32 = new System.Windows.Forms.Button();
+            this.button37 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(857, 39);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Daxil olun";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(418, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(596, 39);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 35);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.YellowGreen;
+            this.button2.Location = new System.Drawing.Point(8, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 51);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sobeleri goster";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGreen;
+            this.button3.Location = new System.Drawing.Point(718, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 48);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Sobe yarat";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(892, 130);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(146, 48);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.Text = "Sobe adini girin";
+            // 
+            // button30
+            // 
+            this.button30.Location = new System.Drawing.Point(12, 213);
+            this.button30.Name = "button30";
+            this.button30.Size = new System.Drawing.Size(152, 42);
+            this.button30.TabIndex = 49;
+            this.button30.Text = "birinci sobe";
+            this.button30.UseVisualStyleBackColor = true;
+            // 
+            // button31
+            // 
+            this.button31.Location = new System.Drawing.Point(205, 213);
+            this.button31.Name = "button31";
+            this.button31.Size = new System.Drawing.Size(152, 42);
+            this.button31.TabIndex = 50;
+            this.button31.Text = "ikinci sobe";
+            this.button31.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Items.AddRange(new object[] {
+            "1111111",
+            "222222",
+            "333333333",
+            "4444444444",
+            "5555555555555555"});
+            this.listBox2.Location = new System.Drawing.Point(12, 320);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(152, 184);
+            this.listBox2.TabIndex = 52;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(34, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 26);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Hekimler";
+            // 
+            // button33
+            // 
+            this.button33.Location = new System.Drawing.Point(12, 522);
+            this.button33.Name = "button33";
+            this.button33.Size = new System.Drawing.Size(152, 43);
+            this.button33.TabIndex = 54;
+            this.button33.Text = "Hekim elave et";
+            this.button33.UseVisualStyleBackColor = true;
+            this.button33.Click += new System.EventHandler(this.button33_Click);
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(12, 582);
+            this.textBox16.Multiline = true;
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(152, 32);
+            this.textBox16.TabIndex = 55;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(12, 696);
+            this.textBox17.Multiline = true;
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(152, 32);
+            this.textBox17.TabIndex = 57;
+            // 
+            // button34
+            // 
+            this.button34.Location = new System.Drawing.Point(12, 636);
+            this.button34.Name = "button34";
+            this.button34.Size = new System.Drawing.Size(152, 43);
+            this.button34.TabIndex = 56;
+            this.button34.Text = "Hekim sil";
+            this.button34.UseVisualStyleBackColor = true;
+            this.button34.Click += new System.EventHandler(this.button34_Click);
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(205, 696);
+            this.textBox18.Multiline = true;
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(152, 32);
+            this.textBox18.TabIndex = 63;
+            // 
+            // button35
+            // 
+            this.button35.Location = new System.Drawing.Point(205, 636);
+            this.button35.Name = "button35";
+            this.button35.Size = new System.Drawing.Size(152, 43);
+            this.button35.TabIndex = 62;
+            this.button35.Text = "Hekim sil";
+            this.button35.UseVisualStyleBackColor = true;
+            this.button35.Click += new System.EventHandler(this.button35_Click);
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(205, 582);
+            this.textBox19.Multiline = true;
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(152, 32);
+            this.textBox19.TabIndex = 61;
+            // 
+            // button36
+            // 
+            this.button36.Location = new System.Drawing.Point(205, 522);
+            this.button36.Name = "button36";
+            this.button36.Size = new System.Drawing.Size(152, 43);
+            this.button36.TabIndex = 60;
+            this.button36.Text = "Hekim elave et";
+            this.button36.UseVisualStyleBackColor = true;
+            this.button36.Click += new System.EventHandler(this.button36_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(227, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 26);
+            this.label4.TabIndex = 59;
+            this.label4.Text = "Hekimler";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Items.AddRange(new object[] {
+            "777777",
+            "888888",
+            "999999",
+            "111111"});
+            this.listBox3.Location = new System.Drawing.Point(205, 320);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(152, 184);
+            this.listBox3.TabIndex = 58;
+            // 
+            // button32
+            // 
+            this.button32.Location = new System.Drawing.Point(15, 745);
+            this.button32.Name = "button32";
+            this.button32.Size = new System.Drawing.Size(149, 45);
+            this.button32.TabIndex = 64;
+            this.button32.Text = "Sobeni sil";
+            this.button32.UseVisualStyleBackColor = true;
+            this.button32.Click += new System.EventHandler(this.button32_Click);
+            // 
+            // button37
+            // 
+            this.button37.Location = new System.Drawing.Point(15, 415);
+            this.button37.Name = "button37";
+            this.button37.Size = new System.Drawing.Size(149, 44);
+            this.button37.TabIndex = 65;
+            this.button37.Text = "Sobeni geri qaytar";
+            this.button37.UseVisualStyleBackColor = true;
+            this.button37.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(1119, 811);
+            this.Controls.Add(this.button37);
+            this.Controls.Add(this.button32);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.button35);
+            this.Controls.Add(this.textBox19);
+            this.Controls.Add(this.button36);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.button34);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.button33);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button31);
+            this.Controls.Add(this.button30);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button30;
+        private System.Windows.Forms.Button button31;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button33;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.Button button34;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.Button button35;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.Button button36;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button32;
+        private System.Windows.Forms.Button button37;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text =="admin")
+            {
+                button2.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Duzgun username ve password girin:");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        TextBox tb;
+        static int a = 480;
+        Button btn = new Button();
+        Label hekimler = new Label();
+        ListBox list1 = new ListBox();
+        Button btn1 = new Button();
+        TextBox hekimyarat = new TextBox();
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            btn.Text = textBox7.Text;
+            btn.Width = 120;
+            btn.Height = 30;
+            btn.BackColor = Color.Aqua;
+            btn.Left = a;
+            btn.Top = 215;
+            Controls.Add(btn);
+   
+            hekimler.Text = "hekimler";
+            hekimler.Top = 280;
+            hekimler.Left = a;
+            Controls.Add(hekimler);
+    
+            list1.Width = 140;
+            list1.Height = 200;
+            list1.Top = 330;
+            list1.Left = a;
+            Controls.Add(list1);
+
+            btn1.Width = 120;
+            btn1.Height = 30;
+            btn1.BackColor = Color.Aqua;
+            btn1.Left = a;
+            btn1.Top = 580;
+            btn1.Click += new EventHandler(this.btn1_Click);
+            Controls.Add(btn1);
+
+            hekimyarat.Width = 120;
+            hekimyarat.Height = 35;
+            hekimyarat.Top = 620;
+            hekimyarat.Left = a;
+            Controls.Add(hekimyarat);
+            a += 300;
+
+        }
+
+        public void btn1_Click(object sender, EventArgs e)
+        {
+            list1.Items.Add(hekimyarat.Text);
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        static int b = 350;
+        private void button22_Click(object sender, EventArgs e)
+        {
+            TextBox hekim = new TextBox();
+            hekim.Top = b;
+            hekim.Left = 714;
+            hekim.Width = 213;
+            Controls.Add(hekim);
+            Button sil = new Button();
+            sil.Width = 62;
+            sil.Left = 630;
+            sil.Top = b;
+            sil.BackColor = Color.Gray;
+            sil.Text = "sil";
+            Controls.Add(sil);
+            b += 40;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.Visible = true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox16.Text);
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Remove(textBox17.Text);
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Add(textBox19.Text);
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Remove(textBox18.Text);
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            button30.Visible = false;
+            button33.Visible = false;
+            button34.Visible = false;
+            button32.Visible = false;
+            listBox2.Visible = false;
+            textBox16.Visible = false;
+            textBox17.Visible = false;
+            label3.Visible = false;
+            button37.Visible = true;
+        }
+    }
+}
